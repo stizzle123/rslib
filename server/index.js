225d77f57264c7dec4bd12d29999911c84ec52d4
Arifayan Idowu Olatubosun <arifayanidowu2@yahoo.com>
2019-12-27 20:28:57 +0100

    Modified index.js

--- server/index.js
+++ server/index.js
@@ -23,45 +23,51 @@ const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  const server = express();
+app
+  .prepare()
+  .then(() => {
+    const server = express();
 
-  server.use(express.json());
-  server.use(express.urlencoded({ extended: false }));
+    server.use(express.json());
+    server.use(express.urlencoded({ extended: false }));
 
-  server.get("/_next/*", (req, res) => {
-    handle(req, res);
-  });
+    server.get("/_next/*", (req, res) => {
+      handle(req, res);
+    });
 
-  server.get("/public/*", (req, res) => {
-    handle(req, res);
-  });
+    server.get("/public/*", (req, res) => {
+      handle(req, res);
+    });
 
-  server.use("/", routes);
+    server.use("/", routes);
 
-  server.get("/service-worker.js", (req, res) => {
-    // Don't cache service worker is a best practice (otherwise clients wont get emergency bug fix)
-    res.set(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    res.set("Content-Type", "application/javascript");
-    if (dev) {
-      app.serveStatic(
-        req,
-        res,
-        path.join(__dirname, "public/service-worker.js")
+    server.get("/service-worker.js", (req, res) => {
+      // Don't cache service worker is a best practice (otherwise clients wont get emergency bug fix)
+      res.set(
+        "Cache-Control",
+        "no-store, no-cache, must-revalidate, proxy-revalidate"
       );
-    } else {
-      app.serveStatic(req, res, path.resolve("./.next/service-worker.js"));
-    }
-  });
+      res.set("Content-Type", "application/javascript");
+      if (dev) {
+        app.serveStatic(
+          req,
+          res,
+          path.join(__dirname, "public/service-worker.js")
+        );
+      } else {
+        app.serveStatic(req, res, path.resolve("./.next/service-worker.js"));
+      }
+    });
 
-  server.get("*", (req, res) => {
-    handle(req, res);
+    server.get("*", (req, res) => {
+      handle(req, res);
+    });
+    server.listen(PORT, err => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${PORT}`);
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
   });
-  server.listen(PORT, err => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${PORT}`);
-  });
-});
